b4ba07b568ef3c5dda3d6744259576ae955d99f7
vivek <vivek_singhai@hotmail.com>
2026-06-29 22:05:15 +0530

    fix build issue.

--- packages/angular/src/public-api.ts
+++ packages/angular/src/public-api.ts
@@ -2,8 +2,15 @@
  * Public API Surface of narrative-form-angular
  */
 
+// Services
 export * from './services/form-state.service';
 export * from './services/toast.service';
+export * from './services/dynamic-form.service';
+
+// Directives
+export * from './directives/typewriter.directive';
+
+// Components
 export * from './components/narrative-form.component';
 export * from './components/line.component';
 export * from './components/welcome-screen.component';
@@ -13,11 +20,19 @@ export * from './components/inline-input.component';
 export * from './components/filled-value.component';
 export * from './components/error-message.component';
 export * from './components/toast-container.component';
+export * from './components/cursor.component';
+export * from './components/edit-icon.component';
+export * from './components/enter-button.component';
+export * from './components/theme-provider.component';
 
-// Export Field Components
+// Field Components
 export * from './components/fields/chips-field.component';
 export * from './components/fields/multi-chips-field.component';
 export * from './components/fields/date-field.component';
 export * from './components/fields/otp-field.component';
 export * from './components/fields/password-field.component';
 export * from './components/fields/select-field.component';
+export * from './components/fields/text-field.component';
+export * from './components/fields/tel-field.component';
+export * from './components/fields/email-field.component';
+export * from './components/fields/number-field.component';
--- packages/vue/src/index.ts
+++ packages/vue/src/index.ts
@@ -3,4 +3,30 @@ export { default as WelcomeScreen } from "./components/WelcomeScreen.vue";
 export { default as DoneScreen } from "./components/DoneScreen.vue";
 export { default as Line } from "./components/Line.vue";
 export { default as ErrorMessage } from "./components/ErrorMessage.vue";
+
+// Structural
+export { default as Cursor } from "./components/Cursor.vue";
+export { default as EditIcon } from "./components/EditIcon.vue";
+export { default as EnterButton } from "./components/EnterButton.vue";
+export { default as FilledValue } from "./components/FilledValue.vue";
+export { default as InlineInput } from "./components/InlineInput.vue";
+export { default as Prose } from "./components/Prose.vue";
+export { default as ThemeProvider } from "./components/ThemeProvider.vue";
+export { default as ToastProvider } from "./components/ToastProvider.vue";
+
+// Fields
+export { default as ChipsField } from "./components/fields/ChipsField.vue";
+export { default as DateField } from "./components/fields/DateField.vue";
+export { default as EmailField } from "./components/fields/EmailField.vue";
+export { default as MultiChipsField } from "./components/fields/MultiChipsField.vue";
+export { default as NumberField } from "./components/fields/NumberField.vue";
+export { default as OtpField } from "./components/fields/OtpField.vue";
+export { default as PasswordField } from "./components/fields/PasswordField.vue";
+export { default as SelectField } from "./components/fields/SelectField.vue";
+export { default as TelField } from "./components/fields/TelField.vue";
+export { default as TextField } from "./components/fields/TextField.vue";
+
+// Composables
 export * from "./composables/useFormState";
+export * from "./composables/useDynamicForm";
+export * from "./composables/useTypewriter";
